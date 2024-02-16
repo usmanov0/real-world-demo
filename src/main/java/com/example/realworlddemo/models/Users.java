e82d19019bb4059b95fb4e3b0b17fcc3d2e69264
usmanov <azikusmanov882@gmail.com>
2024-02-16 12:25:35 +0500

    added :@Builder

--- src/main/java/com/example/realworlddemo/models/Users.java
+++ src/main/java/com/example/realworlddemo/models/Users.java
@@ -2,6 +2,7 @@ package com.example.realworlddemo.models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.Set;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
+@Builder
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
