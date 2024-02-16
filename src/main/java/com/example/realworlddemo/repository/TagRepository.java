9b4c5a63357810442880a289633785c8faa388bb
usmanov <azikusmanov882@gmail.com>
2024-02-16 18:02:45 +0500

    find By name

--- src/main/java/com/example/realworlddemo/repository/TagRepository.java
+++ src/main/java/com/example/realworlddemo/repository/TagRepository.java
@@ -3,7 +3,6 @@ package com.example.realworlddemo.repository;
 import com.example.realworlddemo.models.Tags;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
