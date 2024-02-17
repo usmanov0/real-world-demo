4bd2664c10351c1304d0a1991b29d573011b9720
usmanov <azikusmanov882@gmail.com>
2024-02-17 13:46:50 +0500

    added :ArticleResponse

---
+++ src/main/java/com/example/realworlddemo/dto/response/ArticleResponse.java
@@ -0,0 +1,10 @@
+package com.example.realworlddemo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ArticleResponse{
+    private Articles article;
+}
